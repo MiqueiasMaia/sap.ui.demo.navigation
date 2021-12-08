e765c80748ad5d79378b5a717a00eb89dd903dc9
Miqueias Maia <miqueiasmaia7@gmail.com>
2021-12-08 16:09:29 -0300

    feat: added icon tab bar page to show details

--- uimodule/webapp/controller/employee/Resume.controller.js
+++ uimodule/webapp/controller/employee/Resume.controller.js
@@ -1,18 +1,16 @@
 sap.ui.define([
-    'sap/ui/demo/navigation/controller/BaseController',
+    "sap/ui/demo/navigation/controller/BaseController"
 ], function (BaseController) {
     "use strict";
-
-    return BaseController.extend("sap.ui.demo.navigation.controller.employee.Employee", {
+    return BaseController.extend("sap.ui.demo.navigation.controller.employee.Resume", {
         onInit: function () {
             var oRouter = this.getRouter();
-            oRouter.getRoute("employee").attachMatched(this._onRouteMatched, this);
+            oRouter.getRoute("employeeResume").attachMatched(this._onRouteMatched, this);
         },
         _onRouteMatched: function (oEvent) {
             var oArgs, oView;
             oArgs = oEvent.getParameter("arguments");
             oView = this.getView();
-
             oView.bindElement({
                 path: "/Employees(" + oArgs.employeeId + ")",
                 events: {
@@ -30,12 +28,6 @@ sap.ui.define([
             if (!this.getView().getBindingContext()) {
                 this.getRouter().getTargets().display("notFound");
             }
-        },
-        onShowResume: function (oEvent) {
-            const oContext = this.getView().getBindingContext().getBoundContext();
-            this.getRouter().navTo("employeeResume", {
-                employeeId: oContext.getProperty("EmployeeID")
-            });
         }
     });
 });
